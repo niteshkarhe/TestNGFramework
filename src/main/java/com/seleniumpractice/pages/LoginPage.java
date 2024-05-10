6c4bbc4bada83ae8007081045c35e578f7a58131
niteshkarhe <nitesh.karhe@gmail.com>
2024-05-11 00:20:34 +0530

    Added a log

--- src/main/java/com/seleniumpractice/pages/LoginPage.java
+++ src/main/java/com/seleniumpractice/pages/LoginPage.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.locators.RelativeLocatorServerSide;
 
+import com.aventstack.extentreports.Status;
 import com.seleniumpractice.selenium.SeleniumActions;
+import com.seleniumpractice.selenium.SeleniumTest;
 
 public class LoginPage extends SeleniumActions
 {
@@ -25,6 +27,7 @@ public class LoginPage extends SeleniumActions
 	{
 		try
 		{
+			report.Log("Application URL: " + SeleniumTest.seleniumConfig.getApplicationUrl(), Status.INFO);
 			Thread.sleep(3000);
 			enterText(locators.get("usernameTextBoxName"), "name", "Admin", 5);
 			enterText(locators.get("passwordTextBoxName"), "name", "admin123", 5);
