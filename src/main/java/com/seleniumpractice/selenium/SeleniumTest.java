1591dd9469c6bb3c226a17d125079e1ab62fa525
niteshkarhe <nitesh.karhe@gmail.com>
2024-05-11 00:19:47 +0530

    Added BeforeClass and AfterClass methods

--- src/main/java/com/seleniumpractice/selenium/SeleniumTest.java
+++ src/main/java/com/seleniumpractice/selenium/SeleniumTest.java
@@ -8,7 +8,9 @@ import java.io.FileReader;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import com.google.gson.Gson;
@@ -26,6 +28,15 @@ public class SeleniumTest extends BaseTest {
 	@Setter
 	static public ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 	private DriverManager factory = null;
+	
+	@BeforeClass
+	public void startClass()
+	{
+		if (seleniumConfig.getBrowserScope().equals("class")) 
+		{
+			startWebDriver();
+		}
+	}
 
 	@BeforeMethod
 	public void startTest()
@@ -43,6 +54,15 @@ public class SeleniumTest extends BaseTest {
 		{
 			stopWebDriver();
 		} 
+	}
+	
+	@AfterClass
+	public void stopClass()
+	{
+		if (seleniumConfig.getBrowserScope().equals("class"))
+		{
+			stopWebDriver();
+		}
 	}
 	
 	private void startWebDriver() {
